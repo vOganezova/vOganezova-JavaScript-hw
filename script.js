ad6f90e286b58eff986e983b2340d4b73ef4bb82
NinoPotskhoraia <victoriaoganezova1@gmail.com>
2022-11-06 22:05:22 +0400

    did 3 tasks

---
+++ script.js
@@ -0,0 +1,32 @@
+//first task
+
+let user = {
+    firstname: "giorgi",
+    lastname: "smith",
+    age: 25,
+    studentstatus: "active"
+  };
+
+  console.log(user.studentstatus);
+
+//second task
+
+let nums = [1,2,3,4,5,6,7,8,9];
+for(let i = 0; i<nums.length; i++){
+    console.log(nums[i]);
+}
+
+let i = 0;
+while(i < nums.length){
+    console.log(nums[i]);
+    i++;
+}
+
+//third task
+
+let numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
+for(let i = 0; i < numbers.length; i++){
+    if(numbers[i] > 5){
+        console.log(numbers[i]);
+    }
+}
